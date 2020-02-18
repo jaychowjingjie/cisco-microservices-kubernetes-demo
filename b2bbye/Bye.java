1026299163dce1397e7a152fffac846d4b524742
Jay Chow <jaychow@cisco.com>
2020-02-18 13:35:49 -0800

    for open source demo purposes

---
+++ b2bbye/Bye.java
@@ -0,0 +1,7 @@
+class Bye {
+  public static void main(String[] args) {
+    while(true) {
+      System.out.println("This is java app \n by using Docker. Bye World!");
+    }
+  }
+}  
---
+++ b2bhello/Hello.java
@@ -0,0 +1,7 @@
+class Hello {
+  public static void main(String[] args) {
+    while(true) {
+      System.out.println("This is java app \n by using Docker. Hello World!");
+    }
+  }
+}  
